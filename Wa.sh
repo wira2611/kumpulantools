10b213787b0ff0f6e384a6ef374d5600ca6d42c4
cyber2611 <52934500+cyber2611@users.noreply.github.com>
2019-08-15 04:16:08 +0700

    Add files via upload

---
+++ Wa.sh
@@ -0,0 +1,200 @@
+#!/system/bin/sh
+
+clear
+sleep 1
+echo "╔══╗╔═╗╔═╗╔══╗╔╗╔╗╔═╗╔═╗
+║╔╗║║╬║║║║╚╗╔╝║╚╝║║╦╝║╬║
+║╔╗║║╗╣║║║─║║─║╔╗║║╩╗║╗╣
+╚══╝╚╩╝╚═╝─╚╝─╚╝╚╝╚═╝╚╩╝" | lolcat
+sleep 2
+echo "╒═════════════════════╕
+ *꧁͜͡꧂Selamat Datang꧁͜͡꧂*
+╘═════════════════════╛" | lolcat
+sleep 1
+echo "
+█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+█ █████████                               █
+█ █▄█████▄█ ╔═══╗ ╔═══╗ ╔═══╗ ╔═══╗ ╔╗╔═╗ █
+█ █▼▼▼▼▼    ║╔═╗║ ║╔═╗║ ║╔═╗║ ║╔═╗║ ║║║╔╝ █
+█ █         ║║ ╚╝ ║╚═╝║ ║║ ║║ ║║ ╚╝ ║╚╝╝  █
+█ █▲▲▲▲▲    ║║ ╔╗ ║╔╗╔╝ ║╚═╝║ ║║ ╔╗ ║╔╗║  █
+█ █████████ ║╚═╝║ ║║║╚╗ ║╔═╗║ ║╚═╝║ ║║║╚╗ █
+█  ██ ██    ╚═══╝ ╚╝╚═╝ ╚╝ ╚╝ ╚═══╝ ╚╝╚═╝ █
+█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
+\033[1;91m╔═════════════════════════════════════════════╗
+\033[1;91m║\033[1;93m* \033[1;97mAuthor  \033[1;91m: \033[1;33mMr.Yoga \033[1;91m                         ║
+\033[1;91m║\033[1;93m* \033[1;97mGitHub  \033[1;91m: \033[1;92mhttps//:github.com/cyber2611  \033[1;91m   ║
+\033[1;94m║\033[1;93m* \033[1;97mSupport \033[1;91m: \033[1;98mTeam  \033[1;95m Keluarga  \033[1;96m Allah SWT  \033[1;95m    ║
+\033[1;94m╚═══════════════════════\033[1;95m══════════════════════╝"
+sleep 2
+##############################
+
+##############################
+
+
+echo "[#] Keluar ctrl+c exit...."
+echo "[#] Jangan Di Recode"
+sleep 1
+echo""
+echo "▀▄▀▄▀▄ Brother Victory ▄▀▄▀▄▀"
+
+echo "Yoga Wira"
+sleep 1
+
+echo "DAFTAR PILIHAN TOOLS INSTALLER " | lolcat
+echo "1 Dark3           √" | lolcat
+echo "⋇⊶⊰❣⊱⊷⋇⋇⊶⊰❣⊱⊷⋇⋇⊶⊰⊱" | lolcat
+echo "2 diamond-1       √" | lolcat
+echo "⋇⊶⊰❣⊱⊷⋇⋇⊶⊰❣⊱⊷⋇⋇⊶⊰⊱" | lolcat
+echo "3 Hack2.4         √" | lolcat
+echo "⋇⊶⊰❣⊱⊷⋇⋇⊶⊰❣⊱⊷⋇⋇⊶⊰⊱" | lolcat
+echo "4 Dark-Call       √" | lolcat
+echo "⋇⊶⊰❣⊱⊷⋇⋇⊶⊰❣⊱⊷⋇⋇⊶⊰⊱" | lolcat
+echo "5 Auto Mbf        √" | lolcat
+echo "⋇⊶⊰❣⊱⊷⋇⋇⊶⊰❣⊱⊷⋇⋇⊶⊰⊱" | lolcat
+echo "6 santet-online   √" | lolcat
+echo "⋇⊶⊰❣⊱⊷⋇⋇⊶⊰❣⊱⊷⋇⋇⊶⊰⊱"
+echo "7 Dark Unfriend   √" | lolcat
+echo "⋇⊶⊰❣⊱⊷⋇⋇⊶⊰❣⊱⊷⋇⋇⊶⊰⊱"
+echo "8 Santri-Idn      √" | lolcat
+echo "⋇⊶⊰❣⊱⊷⋇⋇⊶⊰❣⊱⊷⋇⋇⊶⊰⊱"
+echo "9 Crak-Fb         √" | lolcat
+echo "⋇⊶⊰❣⊱⊷⋇⋇⊶⊰❣⊱⊷⋇⋇⊶⊰⊱"
+echo "10 Dark-Naga      √" | lolcat
+echo""
+echo "[00].exit"
+read -p "pilih nomor |===> " pill;
+
+case $pill in
+1)clear
+pkg update
+pkg upgrade
+pkg install python2
+pip2 install mechanize
+pip2 install requests
+pkg install git
+git clone https://github.com/cyber2611/dark3
+ls
+cd dark3
+ls
+python2 coding2.py
+
+;;
+
+2)clear
+pkg install python2
+pip2 install mechanize
+pip2 install requests
+pkg install git
+git clone https://github.com/wira2611/diamon
+ls
+cd diamon
+ls
+python2 diamond-1.py
+
+;;
+
+3)clear
+pkg upgrade && pkg update
+pkg install python2 
+pip2 install requests
+pip2 install mechanize
+pkg install git
+git clone https://github.com/cyber2611/Mr.yoga
+ls
+cd Mr.yoga
+ls
+python2 hack2.4.py
+
+;;
+
+4)clear
+pkg install python2
+pip2 install requests
+pip2 install mechanize
+pkg install git
+git clone https://github.com/wira2611/Dark-Call
+ls
+cd Dark-Call
+python2 Github.py
+
+;;
+
+5)clear
+pkg update && pkg upgrdae -y
+pkg install python2 -y
+pkg install git -y
+pip2 install requests
+pip2 install mechanize
+git clone https://github.com/unikers71/autombf
+cd autombf
+python2 mbf.py
+
+;;
+
+6)clear
+apt update && apt upgrade -y
+apt install python2
+apt install git
+git clone https://github.com/unikers71/santet-online
+cd santet-online
+python2 santet.py
+
+;;
+
+7)clear
+apt update && apt upgrade -y
+apt inatall python2 -y
+apt install git -y
+git clone https://github.com/Unikers71/unfriend
+cd unfriend
+python2 unfriend.py
+
+;;
+
+8)clear
+apt update 
+apt upgrade
+pkg install git 
+pkg install python
+apt install python2 
+pkg install pip 
+pkg install requests 
+pkg install mechanize 
+pip install --upgrade pip 
+git clone https://github.com/SantriCyber/santri-IDN 
+ls
+cd santri-IDN
+python2 santri-IDNv2.py
+
+;;
+
+9)clear
+pkg install python2
+pkg install git
+pip2 install requests
+pip2 install mechanize
+git clone https://github.com/EgiRecod/crackfb
+cd crackfb
+python2 crack.py
+
+;;
+
+10)clear
+pkg install git
+git clone https://github.com/cyber2611/Cyber26-Mod
+ls
+cd Cyber26-Mod
+ls
+python2 Dark-Mod.py
+
+;;
+
+00)clear
+exit
+echo "Add Facebook Gua √"
+exit
+
+esac
+done
+done
